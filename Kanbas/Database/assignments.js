ec4ac710b13c9a97a7fc659725e49c3afa779d59
Mark Panaro <72929673+markpanaro@users.noreply.github.com>
2024-11-17 23:26:12 -0500

    a5 4.2.1

---
+++ Kanbas/Database/assignments.js
@@ -0,0 +1,101 @@
+export default [
+    {
+        "_id": "A101",
+        "title": "Propulsion Assignment",
+        "course": "RS101",
+        "available": "May 6 at 12:00am",
+        "due": "May 12 at 11:59pm",
+        "points": "100",
+        "available_raw": "2024-05-06",
+        "due_raw": "2024-05-12",
+        "description": "The assignment is available online. Submit a link to the landing page..."
+    },
+    {
+        "_id": "A102",
+        "title": "Combustion Analysis",
+        "course": "RS101",
+        "available": "May 8 at 12:00am",
+        "due": "May 12 at 11:59pm",
+        "points": "80",
+        "available_raw": "2024-05-08",
+        "due_raw": "2024-05-12",
+        "description": "The assignment is available online. Only a link needs to be submitted."
+    },
+    {
+        "_id": "A103",
+        "title": "Nozzle Design Project",
+        "course": "RS101",
+        "available": "May 11 at 12:00am",
+        "due": "May 15 at 11:59pm",
+        "points": "100",
+        "available_raw": "2024-05-11",
+        "due_raw": "2024-05-15",
+        "description": "The assignment is available online. Submit a link to the landing page..."
+    },
+    {
+        "_id": "A201",
+        "title": "Aerodynamics Quiz",
+        "course": "RS102",
+        "available": "May 6 at 12:00am",
+        "due": "May 12 at 11:59pm",
+        "points": "100",
+        "available_raw": "2024-05-06",
+        "due_raw": "2024-05-12",
+        "description": "The assignment is available online. Submit a link to the landing page..."
+    },
+    {
+        "_id": "A202",
+        "title": "Flow Analysis",
+        "course": "RS102",
+        "available": "May 7 at 12:00am",
+        "due": "May 12 at 11:59pm",
+        "points": "100",
+        "available_raw": "2024-05-07",
+        "due_raw": "2024-05-12",
+        "description": "The assignment is available online. Submit a link to the landing page..."
+    },
+    {
+        "_id": "A203",
+        "title": "Heating Analysis",
+        "course": "RS102",
+        "available": "May 6 at 12:00am",
+        "due": "May 18 at 11:59pm",
+        "points": "100",
+        "available_raw": "2024-05-06",
+        "due_raw": "2024-05-18",
+        "description": "The assignment is available online. Submit a link to the landing page..."
+    },
+    {
+        "_id": "A301",
+        "title": "Structural Design Task",
+        "course": "RS103",
+        "available": "May 8 at 12:00am",
+        "due": "May 11 at 11:59pm",
+        "points": "100",
+        "available_raw": "2024-05-08",
+        "due_raw": "2024-05-11",
+        "description": "The assignment is available online. Submit a link to the landing page..."
+    },
+    {
+        "_id": "A302",
+        "title": "Orbital Calculations",
+        "course": "RS103",
+        "available": "May 7 at 12:00am",
+        "due": "May 12 at 11:59pm",
+        "points": "100",
+        "available_raw": "2024-05-07",
+        "due_raw": "2024-05-12",
+        "description": "The assignment is available online. Submit a link to the landing page..."
+    },
+    {
+        "_id": "A303",
+        "title": "Systems Engineering Exam",
+        "course": "RS103",
+        "available": "May 9 at 12:00am",
+        "due": "May 14 at 11:59pm",
+        "points": "100",
+        "available_raw": "2024-05-09",
+        "due_raw": "2024-05-14",
+        "description": "The assignment is available online. Submit a link to the landing page..."
+    }
+];
