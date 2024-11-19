fea734ae4448b37f82b09a311922698c66f4f680
Mark Panaro <72929673+markpanaro@users.noreply.github.com>
2024-11-19 14:18:41 -0500

    a5 4.4

--- App.js
+++ App.js
@@ -3,12 +3,33 @@ import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
 const app = express()
-app.use(cors());
-// Define all routes after:
-app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+}));
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+
+app.use(express.json()); // Define routes after
 UserRoutes(app);
+CourseRoutes(app);
 Lab5(app);
 Hello(app)
 app.listen(process.env.PORT || 4000)
