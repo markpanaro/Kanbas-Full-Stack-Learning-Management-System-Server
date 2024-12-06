475f090355c38c02a26f3708816a48148aba8c3e
Mark Panaro <72929673+markpanaro@users.noreply.github.com>
2024-12-06 15:07:22 -0500

    A6 5.3.4

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -112,6 +112,28 @@ export default function UserRoutes(app) {
         const courses = await enrollmentsDao.findCoursesForUser(uid);
         res.json(courses);
     };
+
+    const enrollUserInCourse = async (req, res) => {
+        let { uid, cid } = req.params;
+        if (uid === "current") {
+            const currentUser = req.session["currentUser"];
+            uid = currentUser._id;
+        }
+        const status = await enrollmentsDao.enrollUserInCourse(uid, cid);
+        res.send(status);
+    };
+    const unenrollUserFromCourse = async (req, res) => {
+        let { uid, cid } = req.params;
+        if (uid === "current") {
+            const currentUser = req.session["currentUser"];
+            uid = currentUser._id;
+        }
+        const status = await enrollmentsDao.unenrollUserFromCourse(uid, cid);
+        res.send(status);
+    };
+    app.post("/api/users/:uid/courses/:cid", enrollUserInCourse);
+    app.delete("/api/users/:uid/courses/:cid", unenrollUserFromCourse);
+
     app.get("/api/users/:uid/courses", findCoursesForUser);
 
     app.post("/api/users/current/courses", createCourse);
