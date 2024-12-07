5b72797011c69d867d1c4bfe861ca176d4fedb2b
Mark Panaro <72929673+markpanaro@users.noreply.github.com>
2024-12-06 23:02:03 -0500

    Add fields to assignments

--- Kanbas/Assignments/schema.js
+++ Kanbas/Assignments/schema.js
@@ -3,6 +3,10 @@ const schema = new mongoose.Schema(
   {
     title: String,
     course: { type: mongoose.Schema.Types.ObjectId, ref: "AssignmentModel" },
+    description: String,
+    points: Number,
+    available: Date,
+    due: Date,
   },
   { collection: "assignments" }
 );
