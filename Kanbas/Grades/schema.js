6a0ee1cad6f20632ea89f27b34f91a72864e8e08
Mark Panaro <72929673+markpanaro@users.noreply.github.com>
2024-12-11 11:28:10 -0500

    Add remainingAttempts attribute to Grades schema

--- Kanbas/Grades/schema.js
+++ Kanbas/Grades/schema.js
@@ -13,6 +13,7 @@ const schema = new mongoose.Schema(
         */
         answers: mongoose.Schema.Types.Mixed,
         score: Number,
+        remainingAttempts: Number,
     },
     { collection: "grades" }
 );
